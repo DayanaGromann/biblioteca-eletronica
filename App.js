14069ce76acb14a32126d6a818d1043a7c38bb5a
Dayana <dayana_marcia@hotmail.com>
2022-05-12 21:58:10 -0300

    projeto completo

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './components/BottomTabNavigator';
+import { Rajdhani_600SemiBold } from '@expo-google-fonts/rajdhani';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Login from './screens/Login';
+import {createSwitchNavigator, createAppContainer} from "react-navigation"
+
+export default class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      fonteCarregada: false
+    }
+  }
+  carregarFontes = async()=> {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    })
+
+    this.setState({
+      fonteCarregada: true
+    })
+  }
+
+  componentDidMount(){
+    this.carregarFontes();
+  }
+
+  render(){
+    const {fonteCarregada} = this.state;
+
+    if(fonteCarregada){
+      return (
+        <AppContainer/>
+      );
+    }
+    else{
+      return null;
+    }
+    
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppSwitchNavigator = createSwitchNavigator(
+  {
+    Login: {
+      screen: Login
+    },
+    BottomTab: {
+      screen: BottomTabNavigator
+    }
   },
-});
+  {
+    initialRouteName: "Login"
+  }
+  
+)
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
+
+
